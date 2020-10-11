be2a8ae27b4d822522e6e66b0a53275e1ad99db6
Alice Yeh <aliceyeh12@gmail.com>
2020-10-11 12:50:10 -0700

    Add plugin

--- babel.config.js
+++ babel.config.js
@@ -3,15 +3,8 @@ module.exports = function(api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-<<<<<<< HEAD
-<<<<<<< HEAD
+      "module:react-native-dotenv",
       ["import", { libraryName: "@ant-design/react-native" }]
-=======
-      "module:react-native-dotenv"
->>>>>>> Create nutrition page
-=======
-      "module:react-native-dotenv"
->>>>>>> 7dbb2f0fae3ddeb6bf6229af125c87d75c24629c
     ]
   };
 };

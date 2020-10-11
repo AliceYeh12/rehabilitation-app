5ec03feb18c6ef58d6d5c0a88aa045ac00c52ba2
jjjpanda <jawpanda13@gmail.com>
2020-10-11 01:20:08 -0400

    sleep pulled from google fit

--- components/GoogleSleep.js
+++ components/GoogleSleep.js
@@ -1,31 +1,47 @@
 import GoogleFit, { Scopes } from 'react-native-google-fit'
+import moment from 'moment'
 
-GoogleFit.checkIsAuthorized().then(() => {
-    console.log(GoogleFit.isAuthorized) 
-    if(!GoogleFit.isAuthorized){
-      // The list of available scopes inside of src/scopes.js file
-    const options = {
-        scopes: [
-            Scopes.FITNESS_ACTIVITY_READ_WRITE,
-            Scopes.FITNESS_BODY_READ_WRITE,
-        ],
-    }
-    GoogleFit.authorize(options)
-        .then(authResult => {
-            if (authResult.success) {
-                console.log("AUTH_SUCCESS");
-                GoogleFit.getSleepData({
-                    startDate: "2020-01-01T12:33:18.873Z",
-                    endDate: "2020-10-10T13:45:00.000Z"
-                }, (err, res) => {
-                    console.log(res)
-                });
-            } else {
-                console.log("AUTH_DENIED", authResult.message);
-            }
-        })
-        .catch(() => {
-            console.log("AUTH_ERROR");
-        })
-    }
-})
+const options = {
+    scopes: [
+        Scopes.FITNESS_ACTIVITY_READ_WRITE,
+        Scopes.FITNESS_BODY_READ_WRITE,
+    ],
+}
+
+const parse = (start, end, data) => {
+    console.log(data)
+    return data
+}
+
+const getData = (start, end, callback) => {
+    GoogleFit.getSleepData({
+        startDate: start,
+        endDate: end
+    }, (err, res) => {
+        console.log("Got Sleep Data")
+        callback(err, parse(start, end, res))
+    });
+}
+
+export default function getSleepData(start, end, callback){
+    GoogleFit.checkIsAuthorized().then(() => {
+        console.log(GoogleFit.isAuthorized) 
+        if(!GoogleFit.isAuthorized){  
+            GoogleFit.authorize(options)
+                .then(authResult => {
+                    if (authResult.success) {
+                        console.log("AUTH_SUCCESS");
+                        getData(start, end, callback)
+                    } else {
+                        console.log("AUTH_DENIED", authResult.message);
+                    }
+                })
+                .catch(() => {
+                    console.log("AUTH_ERROR");
+                })
+        }
+        else{
+            getData(start, end, callback)
+        }
+    })
+}
--- components/SleepAnalysis.js
+++ components/SleepAnalysis.js
@@ -8,14 +8,101 @@ import {
   Button
 } from "react-native";
 
+import moment from 'moment'
+import CalendarDay from 'react-native-calendar-strip'
+import TimeInterval from "react-native-clock-interval";
+import getSleepData from "./GoogleSleep";
+
 class SleepAnalysis extends React.Component{
     constructor(props){
         super(props)
+        this.state = {
+            sleepData: [],
+            markedDatesArray: []
+        }
+        this.calendar = React.createRef()
+    }
+
+    componentDidMount(){
+        console.log('Mounted')
+        
+    }
+
+    dateSelected = (date) => {
+        console.log(date)
+    }
+
+    weekSelected = (start, end) => {
+        console.log("WEEK",start, end)
+        getSleepData(moment(start).subtract(1, "week").toISOString(), moment(end).add(1, "week").toISOString(), (err, res) => {
+            
+            if(!err){
+                let marks = []
+                for(const sleepLog of res){
+                    if(moment(sleepLog.endDate).diff(moment(sleepLog.startDate), "minutes") >= 480){
+                        marks.push({
+                            date: moment(sleepLog.endDate),
+                            lines: [{
+                                color: "#32CD32"
+                            }]
+                        })
+                    }
+                    else{
+                        marks.push({
+                            date: moment(sleepLog.endDate),
+                            lines: [{
+                                color: "#FF5555"
+                            }]
+                        })
+                    }
+                }
+                this.setState(() => ({
+                    sleepData: res,
+                    markedDatesArray: marks,
+                }), () => {
+                    console.log("STATE CHANGED")
+                })
+            }
+            else{
+                this.setState(() => ({
+                    sleepData: [],
+                    markedDatesArray: [],
+                }), () => {
+                    console.log("STATE CHANGED")
+                })
+            }
+        })
+    }
+
+    renderRange =() => {
+        
+        const index = this.state.sleepData.findIndex((log) => {
+            return moment(log.endDate).isSame(this.calendar.current.getSelectedDate(), "day")
+        })
+        console.log(index)
+        return (index != -1 ? <View>
+            <Text>
+                Slept From: {moment(this.state.sleepData[index].startDate).format("hh:mm a")}
+            </Text>
+            <Text>
+                To: {moment(this.state.sleepData[index].endDate).format("hh:mm a")}
+            </Text>
+        </View> : null)
     }
 
     render() {
         return (
-            <Button title={"Sleep"} />
+            <View>
+                <CalendarDay 
+                    ref={this.calendar}
+                    onDateSelected = {this.dateSelected}
+                    onWeekChanged = {this.weekSelected}
+                    markedDates = {[...this.state.markedDatesArray]}
+                    maxDate={moment()}
+                />
+                {this.state.sleepData.length > 0 && this.calendar.current.getSelectedDate() ? this.renderRange() : null}
+            </View>
+            
         )
     }
 }
